25dcba3968447d8bad51bed8dca5eb305128d067
David Banks <dave@hoglet.com>
2016-04-23 12:40:09 +0100

    Improved DIN using length detection
    Change-Id: I0445fc1d6ef73d3327d3cd1dcf50d9d59b8b37c8

--- src/cli.asm
+++ src/cli.asm
@@ -7,7 +7,7 @@ STACKPOINTER    = $4                 ; STANDARD ATOM ADDRESS
 STACK1          = $16
 
 .macro FNADDR addr
-   .byte $80 + >addr, <addr
+   .byte <($80 + >addr), <addr
 .endmacro
 
 ;=================================================================
@@ -188,6 +188,7 @@ sd_mount:
    pha                          ; Save drive num
    jsr   prepare_write_data     ; Reset globalbufferpointer
    pla                          ; Restore drive num
+   sta tmp_drive		; Save drivenr
 
    ldx   #0                     ; Globalbuffer(0)=drive num
 loop_m:
@@ -204,13 +205,30 @@ loop_m:
    jsr   slow_cmd
    cmp   #$40
    bcs   bad_image
+
+   jsr star_fatinfo		; Get disksize index
+   cpx #$ff
+   beq unmount
+
+   ldy tmp_drive		; Put disk params in act_disk table
+   lda disk_sect,x
+   sta act_disk_sect,y
+   lda disk_int,x
+   sta act_disk_int,y
+
    rts
+
+unmount:
+   lda tmp_drive		; Wrong size, unmount disk
+   jsr sd_unmount
 
 bad_image:
    jsr   STROUT                 ; print error
    .byte "IMAGE?"
    nop
    brk
+
+tmp_drive:	.byte 0
         
 ;-----------------------------------------------------------------
 ; sd_unmount
@@ -269,3 +287,227 @@ read_num:
 chk_end_command:
    ldy   $3
    jmp   COS_POST_TEST
+
+;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~
+;
+; *FATINFO [filename]
+;
+; Shows fat filesystem file info - size on disk, sector, fptr and attrib.
+;
+star_fatinfo:
+
+
+;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~
+;
+; opens a file for reading, then gets the file info
+;
+; this is used by fatinfo, exec, and rload
+
+open_filename_getinfo:
+   jsr   open_file_read		; invokes error handler if return code > 64
+
+   jsr   set_rwptr_to_name	; get the FAT file size - text files won't have ATM headers
+   lda   #CMD_FILE_GETINFO
+   jsr   slow_cmd
+
+   ldx   #13			; Read FAT data
+   jsr read_data_buffer
+
+   ldx  #$ff			; Check if disksize in table
+print_loop:
+   inx
+   lda table_disksize_lb,x
+   cmp #$ff
+   beq not_found_error
+   cmp NAME+1
+   bne print_loop
+
+   lda table_disksize_hb,x	; lb found, check hb
+   cmp NAME+2
+   bne print_loop
+
+disk_found:
+   rts				; Disksize in table, return index in X
+
+not_found_error:
+   ldx #$ff			; Disksize not found, return $ff in X
+   rts
+   
+;----------------------------------------------------
+; Disksize table
+;
+; Atom disksize SD SS 40tr 10sect = 100 KB ($019000)
+; BBC disksize  SD DS 80tr 10sect = 400 KB ($064000)
+;----------------------------------------------------
+
+table_disksize_hb:	.byte $01,$06,$ff	; hb disksize/256
+table_disksize_lb:	.byte $90,$40,$ff	; lb disksize/256
+
+;---------------------------------------
+; Disktype parameters  0,  1
+;---------------------------------------
+
+disk_sect:	.byte 10, 20	; Number of sectors per (image) track
+disk_int:	.byte  0, 10	; Number of sectors to skip to read interleaved data
+
+;---------------------------------------
+; Drive parameters     0,  1,  2,  3
+;---------------------------------------
+
+act_disk_sect:	.byte  0,  0,  0,  0
+act_disk_int:	.byte  0,  0,  0,  0
+
+;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~
+;
+; Read data to memory from the pic's buffer
+;
+; data may be from another source other than file, ie getfileinfo
+; x = number of bytes to read (0 = 256)
+; (RWPTR) points to store
+;
+read_data_buffer:
+   jsr   prepare_read_data
+
+   ldy   #0
+
+@loop:
+   jsr   read_data_reg
+   sta   (RWPTR),y
+   iny
+   dex
+   bne   @loop
+
+return_ok:
+   rts
+
+;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~
+;
+; set RWPTR to point to NAME  (i.e. $140)
+;
+; this is called 5 times, so making it a subroutine rather than a macro
+; saves 4 * (8 - 3) - 9 = 11 bytes!
+set_rwptr_to_name:
+   lda   #<NAME
+   sta   RWPTR
+   lda   #>NAME
+   sta   RWPTR+1
+   rts
+
+;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~
+;
+; Read filename, then fall through to open_file_read
+
+open_filename_read:
+   jsr   read_filename          ; copy filename from $100 to $140
+   ; fall through to open_file_read 
+
+;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~
+;
+; Open file for read or write
+;
+; $140 = name
+; a = read/write $01 = read, $11 = write
+;
+
+open_file_read:
+   lda   #CMD_FILE_OPEN_READ
+   jsr   open_file
+   jmp   expect64orless
+
+open_file_write:
+   lda   #CMD_FILE_OPEN_WRITE
+
+; Falls through to
+open_file:
+   pha
+   jsr   send_name
+   pla
+   jmp   slow_cmd
+
+send_name:
+   jsr   prepare_write_data
+
+send_additional_name:
+   ldx   #0
+   beq   @pumpname
+
+@nextchar:
+   jsr   write_data_reg
+   inx
+
+@pumpname:
+   lda   NAME,x			; write filename to filename buffer
+   cmp   #$0d
+   bne   @nextchar
+
+   lda   #0			; terminate the string
+   jmp   write_data_reg
+
+expect64orless:
+   cmp   #STATUS_COMPLETE+1
+   bcc   return_ok
+   ; fall through to report_disk_failure
+
+
+;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~;~~
+;
+; report a file system error
+;
+report_disk_failure:
+   and   #ERROR_MASK
+   pha                          ; save error code
+   tax                          ; error code into x
+   ldy   #$ff                   ; string indexer
+
+@findstring:
+   iny                          ; do this here because we need the z flag below
+   lda   diskerrortab,y
+   bne   @findstring            ; zip along the string till we find a zero
+
+   dex                          ; when this bottoms we've found our error
+   bne   @findstring
+   pla                          ; restore error code
+   tax                          ; error code in X
+   lda   TUBE_FLAG
+   cmp   #TUBE_ENABLED
+   beq   @tubeError
+
+@printstring:
+   iny
+   lda   diskerrortab,y
+   jsr   OSWRCH
+   bne   @printstring
+   brk
+
+@tubeError:
+   iny                          ; store index for basic BRK-alike hander
+   tya
+   clc
+   adc   #<diskerrortab
+   sta   $d5
+   lda   #>diskerrortab
+   adc   #0
+   sta   $d6
+   jmp   L0409                  ; error code in X (must be non zero)
+
+diskerrortab:
+   .byte $00
+   .byte "DISK FAULT",$00
+   .byte "INTERNAL ERROR",$00
+   .byte "NOT READY",$00
+   .byte "NOT FOUND",$00
+   .byte "NO PATH",$00
+   .byte "INVALID NAME",$00
+   .byte "ACCESS DENIED",$00
+   .byte "EXISTS",$00
+   .byte "INVALID OBJECT",$00
+   .byte "WRITE PROTECTED",$00
+   .byte "INVALID DRIVE",$00
+   .byte "NOT ENABLED",$00
+   .byte "NO FILESYSTEM",$00
+   .byte $00                    ; mkfs error
+   .byte "TIMEOUT",$00
+   .byte "EEPROM ERROR",$00
+   .byte "FAILED",$00
+   .byte "TOO MANY",$00
+   .byte "SILLY",$0d
